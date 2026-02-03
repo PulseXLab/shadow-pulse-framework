b9cfe53fb7324d605b5da3dc4f206bfb20187a3a
PulseX RedTeam <info@pulsex.kr>
2026-02-03 12:13:38 +0900

    update utils.SanitizeFilename function and copile error...

--- internal/utils/filenames.go
+++ internal/utils/filenames.go
@@ -2,7 +2,7 @@ package utils
 
 import "strings"
 
-// SanitizeURL creates a valid filename from a URL string.
+// SanitizeURL creates a valid filename from a URL string for image files.
 func SanitizeURL(rawURL string) string {
 	// Remove protocol scheme
 	sanitized := strings.Replace(rawURL, "https://", "", 1)
@@ -11,4 +11,27 @@ func SanitizeURL(rawURL string) string {
 	sanitized = strings.ReplaceAll(sanitized, ":", "_")
 	sanitized = strings.ReplaceAll(sanitized, "/", "")
 	return sanitized + ".png"
+}
+
+// SanitizeFilename creates a safe filename by replacing invalid characters.
+func SanitizeFilename(filename string) string {
+	// Remove protocol schemes
+	sanitized := strings.Replace(filename, "https://", "", 1)
+	sanitized = strings.Replace(sanitized, "http://", "", 1)
+
+	// Replace common invalid characters with an underscore
+	replacer := strings.NewReplacer(
+		"/", "_",
+		":", "_",
+		"?", "_",
+		"&", "_",
+		"=", "_",
+		"\\", "_",
+		"*", "_",
+		"<", "_",
+		">", "_",
+		"|", "_",
+		"\"", "_",
+	)
+	return replacer.Replace(sanitized)
 }
